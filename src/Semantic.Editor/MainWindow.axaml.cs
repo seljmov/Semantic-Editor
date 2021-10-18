1d25bec72de06bd6f46dca9c7bf7577fbcd88fd1
Zagir Selimov <seljmov@list.ru>
2021-10-18 18:38:19 +0400

    Инициализация проекта

---
+++ src/Semantic.Editor/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Semantic.Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
